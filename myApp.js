0c269e0ab455064f0ba5feda5f20558b40c380f2
nasernikzad <n.nikzad@pomtech.com>
2021-05-24 08:47:54 +0430

    changes 2

--- myApp.js
+++ myApp.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
