2970338c9ea0ce0f8dd0ff738bc42f16aa653f59
nasernikzad <n.nikzad@pomtech.com>
2021-05-24 09:12:47 +0430

    asdf

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 console.log(123);
 console.log(process.env.MONGO_URI);
 
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
