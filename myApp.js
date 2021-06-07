af29b9f383a4fa444d11f5254491d51b52e471b9
nasernikzad <n.nikzad@pomtech.com>
2021-06-07 08:59:24 +0430

    changes

--- myApp.js
+++ myApp.js
@@ -105,7 +105,7 @@ const queryChain = (done) => {
    Person.find({favoriteFoods: foodToSearch})
     .sort({name:  "asc"})
     .limit(2)
-    .select({name:1, age: 0, favouriteFoods:1})
+    .select("-age")
     .exec(function (err, data) {
     if (err) return console.log(err);
     done(null, data);
