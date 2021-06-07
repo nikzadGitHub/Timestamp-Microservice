8da7246bd6b54cd6062b2d902fa2e0025f0352d1
nasernikzad <n.nikzad@pomtech.com>
2021-06-07 08:22:45 +0430

    changes

--- myApp.js
+++ myApp.js
@@ -95,6 +95,11 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
+  Person.remove ({ name: nameToRemove }, (err, result) => {
+    if(err) return console.log(err);
+    done(null, result);
+  });
+  
   done(null /*, data*/);
 };
 
