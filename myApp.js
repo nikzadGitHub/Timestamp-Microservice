4f70b2f218e6204a4699cbd99934d61c01a36ad7
nasernikzad <n.nikzad@pomtech.com>
2021-06-07 08:58:11 +0430

    changes

--- myApp.js
+++ myApp.js
@@ -103,7 +103,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
    Person.find({favoriteFoods: foodToSearch})
-    .sort({name: -1})
+    .sort({name:  "asc"})
     .limit(2)
     .select({name:1, age: 0, favouriteFoods:1})
     .exec(function (err, data) {
