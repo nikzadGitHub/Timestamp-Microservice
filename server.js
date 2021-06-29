d406933a1f7762993327acb3ad24db69c744f3d3
nasernikzad <n.nikzad@pomtech.com>
2021-06-29 17:21:36 +0430

    changes

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(ignoreFavicon);
 // });
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
