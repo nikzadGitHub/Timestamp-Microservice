fffb91da5407a9d715b99cf76f84eda49a4dcdc8
nasernikzad <n.nikzad@pomtech.com>
2021-06-29 17:25:33 +0430

    changes

--- server.js
+++ server.js
@@ -5,29 +5,59 @@
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var moment = require("./moment.js");
+// var port = Number(process.env.PORT||50893);
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.get('/api/:date?',function(req,res){
+
+    let input = req.params.date;
+
+    let outPut = { error : "Invalid Date" };
+    
+    // Handle empty, get current time.
+    if (typeof input == 'undefined' || input.length == 0) {
+      console.log('undefined or empty')
+      input = moment(); // Get current time
+    }
+
+    console.log('next step')
+
+    if(new Date(input) != 'Invalid Date') {
+
+      console.log('valid input date')
+
+      // Time conversion to unix and utc
+      outPut = { 
+        unix : moment(input).unix(),
+        utc: moment.utc(input).format("ddd, DD MMM YYYY h:mm:ss ") + 'GMT'
+      }
+    }
+    else {
+      // Time conversion to unix and utc
+      outPut = { 
+        unix : input,
+        utc: new Date(input * 1000)
+      }
+    }  
+      res.json(outPut);
+});
+
+app.use(ignoreFavicon);
+// app.use('/',express.static(__dirname+'/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  console.log("I am here2");
-  res.json({greeting: 'hello API'});
-});
-
-
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  }
+  next();
+}
